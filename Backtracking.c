2ab7a2fa910758fa18c70622a853b16812f30ce0
PC_home <sogunul2005@gmail.com>
2026-01-06 19:33:23 +0200

    1

--- Backtracking.c
+++ Backtracking.c
@@ -71,9 +71,9 @@ int main(int argc, char **argv)
         }
     }
 
+    fprintf(o, "%d\n", best_value);
     for (int i = 0; i < best_size; i++)
         fprintf(o, "%d ", best_sol[i]);
-    fprintf(o, "\n");
 
     free(w);
     free(v);
